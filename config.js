1b4c0ac5ba87d093a7f6df573c0e7030b1417db5
agusprana <odhivpds01@gmail.com>
2026-07-24 20:49:51 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * KONFIGURASI CLIENT AFFILIATE
+ * File ini TIDAK AKAN DIACAK, sehingga Client Anda bisa mengedit ini di Github.
+ */
+const CONFIG = {
+    // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
+    API_BASE_URL: 'https://api.getfiles.my.id',
+
+    // URL Direct Link Pop-under Milik Client (Kosongkan jika menggunakan script di index.html)
+    CLIENT_POPUNDER_URL: '',
+
+    // URL CDN (Pull Zone) dari Bunny Storage Anda (Kosongkan jika masih pakai Stream lama)
+    // Contoh: 'cdn.domainanda.com' atau 'nama-zone.b-cdn.net'
+    BUNNY_PULL_ZONE_URL: 'getfiles.b-cdn.net',
+
+    // URL CDN Cloudflare R2 (Baru)
+    R2_PUBLIC_URL: 'pub-1880d72cd0f343999d3197d949ff8213.r2.dev'
+};
